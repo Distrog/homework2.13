270a4eb2438cd50d1428210f8669dfe36456981b
Dmitriy Stroganov <Distrog@gmail.com>
2024-04-22 21:45:30 +0700

    Проверьте ДЗ

---
+++ src/test/java/ru/stroganov/calculator_tests/service/CalculatorServiceTestConstants.java
@@ -0,0 +1,10 @@
+package ru.stroganov.calculator_tests.service;
+
+public class CalculatorServiceTestConstants {
+    public static final Integer NUM1 = 1;
+    public static final Integer NUM2 = 2;
+    public static final Integer NUM3 = -2;
+    public static final Integer NUM4 = -3;
+    public static final Integer ZERO = 0;
+    public static final Integer NUM_NULL = null;
+}
