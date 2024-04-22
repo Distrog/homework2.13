49d42606c640f26c627ea17f4638a93e35a075ef
Dmitriy Stroganov <Distrog@gmail.com>
2024-04-22 20:19:48 +0700

    Initial commit

---
+++ src/main/java/ru/stroganov/calculator_tests/CalculatorTestsApplication.java
@@ -0,0 +1,13 @@
+package ru.stroganov.calculator_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorTestsApplication.class, args);
+	}
+
+}
